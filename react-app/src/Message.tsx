235a3bfc5e2ee9a3422596040844fe91e76695f5
Justin Rivera <159206152+JustinRivera2018@users.noreply.github.com>
2024-02-08 20:07:13 -0500

    update

---
+++ react-app/src/Message.tsx
@@ -0,0 +1,9 @@
+//Function Based Components are recommended
+//PascalCasing Capitalize first letter of every word in REACT
+function Message(){
+    //JSX: JavaScript XML     NOT HTML code
+    return <h1>Hello World</h1>
+
+}
+
+export default Message;
